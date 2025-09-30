131fb7153f21893a34cd97744f9ef343a6886e88
Fatima Al-Zahraa <fatimaalzahraajaffal@gmail.com>
2025-09-30 17:10:10 +0300

    applying discount

--- GroceryProgram.py
+++ GroceryProgram.py
@@ -1,5 +1,5 @@
 total = 0
-cart=[]
+cart={}
 groceries = {
     "apples": 5,
     "banana": 3,
@@ -12,21 +12,35 @@ groceries = {
 
 
 while True:
-    item = input("What do you want to buy? ")
+    cin = input("What do you want to buy? ").lower().strip()
 
-    if item == "done":
+    if cin == "done":
         break
+
+
+    spliting = cin.split()
+    item, quantity = spliting[0], int(spliting[1])
+
     if item in groceries:
-        cart.append(item)
+        if item in cart:
+           cart[item] = cart[item] + quantity
+
+        else:
+            cart[item] = quantity
 
     else:
-        print("Item not available.")
+        print("Sorry, we don’t have that item")
 
-for item in cart:
-    total= total+ groceries[item]
+for item, quantity in cart.items():
+    price = groceries[item]
+    if item == "milk" and quantity > 2:
+        price = price -1  
+    total += price * quantity
+
+
 
 print("you bought: ", cart )
-print("total= ", total)
+print("total = $ ", total)
 
 if total > 10:
     print("You spent a lot!")
